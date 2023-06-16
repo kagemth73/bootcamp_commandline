5355be9a97af28949a6b12df457c249741946f3b
kagemth73 <srjkk8234@yahoo.co.jp>
2023-06-16 18:02:25 +0900

    Add answers to some files and add a sorted csv file

--- answer_find.js
+++ answer_find.js
@@ -1,7 +1,7 @@
 export default {
   // 最も多い行数を持つ「.txt」ファイルへのパス: files_to_search/ からのパスを記述してください
   // 例： files_to_search/dir0/file0.txt
-  largestFilename: "files_to_search/",
+  largestFilename: "files_to_search/dir3/file7.txt",
   // 最も多い行数を持つファイルの行数
-  largestFileLines: 0,
+  largestFileLines: 89,
 };
